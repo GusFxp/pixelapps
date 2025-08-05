d3ebd7ce5d5ffdb537fe5f65863dccf4b48052d3
GusFxp <gustavo2gabriel2@gmail.com>
2025-08-04 23:43:54 -0300

    Atualização

---
+++ particles.js
@@ -0,0 +1,64 @@
+const canvas = document.getElementById("particle-canvas");
+const ctx = canvas.getContext("2d");
+
+let particlesArray = [];
+const particleCount = 80;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.radius = Math.random() * 2 + 1;
+    this.speedX = (Math.random() - 0.5) * 0.5;
+    this.speedY = (Math.random() - 0.5) * 0.5;
+    this.color = "rgba(0, 204, 255, 0.7)";
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    // quicar nas bordas
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.speedX *= -1;
+    }
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.speedY *= -1;
+    }
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.shadowColor = this.color;
+    ctx.shadowBlur = 10;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particlesArray = [];
+  for (let i = 0; i < particleCount; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particlesArray.forEach((p) => {
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+initParticles();
+animate();
